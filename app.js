98d4bf0ae9f29667dc3651767e11044cf58cc574
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-10-26 20:44:24 -0600

    Actualizando imagen según información del API

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const apiKey = "51afb30ca00563e91e6b6d94f858fd2b";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&lang=es&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const ciudadInput = document.querySelector(".search__input");
 const botonBuscar = document.querySelector(".search__btn");
+
+const weatherImage = document.querySelector(".weather__icon");
 
 async function checarClima(ciudadNueva) {
     const respuesta = await fetch(apiUrl + ciudadNueva + `&appid=${apiKey}`);
@@ -18,6 +20,27 @@ async function checarClima(ciudadNueva) {
     document.querySelector(".weather__temp").innerHTML = Math.round(temperatura) + "°C";
     document.querySelector(".humidity").innerHTML = humedad + "%";
     document.querySelector(".wind").innerHTML = velViento + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherImage.src = "./Images/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+        weatherImage.src = "./Images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherImage.src = "./Images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherImage.src = "./Images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherImage.src = "./Images/mist.png";
+    }
+    else if (data.weather[0].main == "Snow") {
+        weatherImage.src = "./Images/snow.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
 }
 
 botonBuscar.addEventListener("click", () => {
